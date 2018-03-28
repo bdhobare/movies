780a4f34fa464985884039ea8f33918b77a03dde
bdhobare <bdhobare@gmail.com>
2018-03-28 14:22:10 +0300

    More travis

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 eval "$(ssh-agent -s)" # Start ssh-agent cache
-chmod 600 travis # Allow read access to the private key
-ssh-add travis # Add the private key to SSH
+chmod 600 laplace # Allow read access to the private key
+ssh-add laplace # Add the private key to SSH
 
 git config --global push.default matching
 git remote add deploy ssh://obare_obadiah@$IP:$PORT$DEPLOY_DIR
